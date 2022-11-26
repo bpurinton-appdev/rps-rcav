0eb30789145f8aae3bd3c3e99dac60e9fc6ce40e
Ben Purinton <ben.purinton@gmail.com>
2022-11-26 21:35:10 +0000

    got the routes and rules setup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,45 @@ class ApplicationController < ActionController::Base
   # Add your actions below this line
   # ================================
 
+  def play_rock
+    @comp_move = ["rock", "paper", "scissors"].sample
+
+    if @comp_move == "rock"
+      @result = "tied"
+    elsif @comp_move == "paper"
+      @result = "lost"
+    elsif @comp_move == "scissors"
+      @result = "won"
+    end
+
+    render({ :plain => "Hello, world!" })
+  end
+
+  def play_paper
+    @comp_move = ["rock", "paper", "scissors"].sample
+
+    if @comp_move == "rock"
+      @result = "won"
+    elsif @comp_move == "paper"
+      @result = "tied"
+    elsif @comp_move == "scissors"
+      @result = "lost"
+    end
+  end
+
+  def play_scissors
+    @comp_move = ["rock", "paper", "scissors"].sample
+
+    if @comp_move == "rock"
+      @result = "lost"
+    elsif @comp_move == "paper"
+      @result = "won"
+    elsif @comp_move == "scissors"
+      @result = "tied"
+    end
+  end
+
+  def homepage
+    render({ :plain => "Hello, world!" })
+  end
 end
